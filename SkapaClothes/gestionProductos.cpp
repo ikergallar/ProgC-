0e9a3772dec11a6db0a8cf8dfa67938c3c45a85e
misaga019 <misaga@opendeusto.es>
2021-05-12 10:03:56 +0200

    gestion de productos creado

---
+++ SkapaClothes/gestionProductos.cpp
@@ -0,0 +1,125 @@
+#include "iostream"
+#include <stdlib.h>
+#include "Producto.h"
+#include "Usuario.h"
+#include "gestionProducto.h"
+
+void imprimirStock (Producto* productos, int cantProductos){
+	for (int i = 0; i < cantProductos; i++){
+		productos[i].imprimirProducto();
+		cout << endl;
+	}
+}
+
+
+void listarProductos (Producto *stock, int cantProducto){
+
+	int num = 1;
+
+	for (int i = 0; i < cantProducto; i++){
+		cout << num << ". " << stock[i].getNombre() << endl;
+		num++;
+	}
+}
+
+void eliminarProducto (Producto * stock, int cantProducto){
+
+	system("cls");
+
+	int op;
+
+	listarProductos(stock, cantProducto);
+
+	cout << "Pulsa el numero del producto que quieras eliminar" << endl;
+	cin >> op;
+
+
+	for (int i = op; i < cantProducto; i++){
+		stock[i-1] = stock[i];
+
+	}
+
+	cantProducto--;
+	exportarStock(stock, cantProducto);
+
+
+}
+
+void anyadirProducto (Producto*stock, int cantProducto){
+
+	system("cls");
+
+	int id;
+	string nombre;
+	string tipo;
+	string descripcion;
+	float precio;
+
+	cout << "Id: " << endl;
+	cin >> id;
+	cout << "Nombre: " << endl;
+	cin >> nombre;
+	cout << "Tipo :" << endl;
+	cin >> tipo;
+	cout << "Descripcion :" << endl;
+	cin >> descripcion;
+	cout << "Precio :" << endl;
+	cin >> precio;
+
+
+	Producto * p = new Producto (id, nombre, tipo, descripcion, precio);
+
+	stock[cantProducto] = *p;
+	cantProducto++;
+
+	exportarStock(stock, cantProducto);
+
+}
+
+void menuGestionarProductos (Producto *stock, int cantProducto){
+
+	int op;
+	char c;
+
+	do{
+
+		cantProducto = importarStock(stock);
+
+		system("cls");
+
+		cout << "Gestion de productos" << endl;
+		cout << "_____________________________" << endl;
+
+		cout << "1. Mostrar los productos disponibles" << endl;
+		cout << "2. Añadir nuevo producto a la tienda" << endl;
+		cout << "3. Eliminar producto existente" << endl;
+		cout << "4. Volver" << endl;
+
+		cin >> op;
+
+		switch (op){
+
+		case 1:
+			listarProductos(stock, cantProducto);
+			cout << "Pusla un numero para continuar..." << endl;
+			cin >> c;
+			break;
+
+		case 2:
+			anyadirProducto(stock, cantProducto);
+			break;
+
+		case 3:
+			eliminarProducto(stock, cantProducto);
+			break;
+
+		default:
+			cout << "Opcion seleccionada incorrecta" << endl;
+			break;
+		}
+
+	}while (op != 4);
+
+
+
+}
